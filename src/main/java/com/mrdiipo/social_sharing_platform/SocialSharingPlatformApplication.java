e277a82507ed42bd7425d92da7c2bb8d370acea7
MrDiipo <68384682+MrDiipo@users.noreply.github.com>
2021-10-27 18:07:06 +0100

    Initial Commit

---
+++ src/main/java/com/mrdiipo/social_sharing_platform/SocialSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.social_sharing_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialSharingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialSharingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrdiipo/social_sharing_platform/SocialSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrdiipo.social_sharing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialSharingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
